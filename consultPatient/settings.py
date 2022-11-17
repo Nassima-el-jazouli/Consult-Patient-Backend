7728800b7dc9796520888e7f2979bd310627337d
Nassima EL JAZOULI <nassima.eljazouli@etu.uae.ac.ma>
2022-11-17 20:42:05 +0100

    Update Kafka Producer & Consumer

--- consultPatient/settings.py
+++ consultPatient/settings.py
@@ -38,12 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'Patient',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -125,3 +127,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://192.168.56.1:3000",
+    "http://localhost:3000"
+]
--- kafka/kafka-consumer.py
+++ kafka/kafka-consumer.py
@@ -1,52 +1,25 @@
+import threading
+from kafka import KafkaConsumer
+from json import loads
+import json
 import pandas as pd
-from kafka import KafkaProducer
-from datetime import datetime
-import time
-import random
-import numpy as np
+#Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age,Outcome
+lst = ['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 
+            'Insulin', 'BMI', 'DiabetesPedigreeFunction','Age','Outcome']
+df = pd.DataFrame(columns = ['preg', 'plas', 'pres','skin','insu','mass','pedi','age','class'])
 
-# pip install kafka-python
+def get_data():
+    consumer = KafkaConsumer('Topic', bootstrap_servers=['localhost:9092']
+    , auto_offset_reset='earliest'
+                         )
 
-KAFKA_TOPIC_NAME_CONS = "Topic"
-KAFKA_BOOTSTRAP_SERVERS_CONS = 'localhost:9092'
-
-if __name__ == "__main__":
-    print("Kafka Producer Application Started ... ")
-
-    kafka_producer_obj = KafkaProducer(bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS_CONS,
-                                       value_serializer=lambda x: x.encode('utf-8'))
-    
-    filepath = "Diabetes.csv"
-    
-    dataset = pd.read_csv(filepath)
+    for message in consumer:
+        global df
   
-    dataset['Outcome'] = np.arange(len(dataset))
-
-    
-    dataset_list = dataset.to_dict(orient="records")
-       
-
-    message_list = []
-    message = None
-    for message in dataset_list:
-        
-        message_fields_value_list = []
-               
-        message_fields_value_list.append(message['Pregnancies'])
-        message_fields_value_list.append(message['Glucose'])
-        message_fields_value_list.append(message['BloodPressure'])
-        message_fields_value_list.append(message['SkinThickness'])
-        message_fields_value_list.append(message['Insulin'])
-        message_fields_value_list.append(message['BMI'])
-        message_fields_value_list.append(message['DiabetesPedigreeFunction'])
-        message_fields_value_list.append(message['Age'])
-        message_fields_value_list.append(message['Outcome'])
-
-        message = ','.join(str(v) for v in message_fields_value_list)
-#         print("Message Type: ", type(message))
-        print("Message: ", message)
-        kafka_producer_obj.send(KAFKA_TOPIC_NAME_CONS, message)
-        time.sleep(0.01)
-
-
-    print("Kafka Producer Application Completed. ")
+        sliced_row=message.value.decode().split(",")
+        try:
+            df.loc[len(df)] = sliced_row
+            print(df.describe())
+        except:
+            print(df.describe())
+get_data()
